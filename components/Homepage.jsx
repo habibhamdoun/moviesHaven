36fad46817501e527bec81e129b0c972faae5aaa
habibhamdoun <100615843+habibhamdoun@users.noreply.github.com>
2023-06-23 22:34:24 +0300

    still trying to fix the fav icon

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,18 +1,40 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Homepage = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (window.screen.width < 768) {
+      setIsMobile(true);
+    }
+  }, []);
   return (
     <section className='h-[100vh] flex flex-col items-center justify-center gap-8 homepage '>
-      <h1 className='text-6xl font-bold'>What are you looking for?</h1>
+      <h1 className={isMobile ? 'text-4xl font-bold' : 'text-6xl font-bold'}>
+        What are you looking for?
+      </h1>
       <div className='flex gap-5 '>
         <Link href={`/movies`}>
-          <h2 className='text-4xl font-extrabold bg-yellow-600  rounded-lg p-2 w-fit cursor-pointer'>
+          <h2
+            className={
+              isMobile
+                ? 'text-2xl font-extrabold bg-yellow-600  rounded-lg p-2 w-fit cursor-pointer'
+                : 'text-4xl font-extrabold bg-yellow-600  rounded-lg p-2 w-fit cursor-pointer'
+            }
+          >
             Movies
           </h2>
         </Link>
         <Link href={`/tv`}>
-          <h2 className='text-4xl font-extrabold bg-yellow-600  rounded-lg p-2 w-fit cursor-pointer'>
+          <h2
+            className={
+              isMobile
+                ? 'text-2xl font-extrabold bg-yellow-600  rounded-lg p-2 w-fit cursor-pointer'
+                : 'text-4xl font-extrabold bg-yellow-600  rounded-lg p-2 w-fit cursor-pointer'
+            }
+          >
             Tv Shows
           </h2>
         </Link>
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
           name='description'
           content='Discover and explore a dynamic movie showcase powered by Next.js and the TMDB API. Experience a constantly updated collection of movies, featuring the latest releases, popular films, and timeless classics. With automatic updates and seamless navigation, this website brings the world of cinema to your fingertips.'
         />
-        <link rel='shortcut icon' href='../favicon.ico' />
       </Head>
       <Homepage />
     </div>
